f93e0d977b35a07048e93e6aecf683f375636c31
DiQU <dieqju@gmail.com>
2019-01-06 04:58:21 +0800

    自動輸出 partB

---
+++ test.js
@@ -0,0 +1,64 @@
+const fs = require('fs')
+fs.readFile('str.txt', 'utf8', (err, file) => {
+	if (err) console.log(err)
+	else
+        outFile=printGitbook(file)
+        fs.writeFile('unit.md',outFile,(err)=>{
+            if(err) console.log(err)
+            else{
+                console.log('寫入成功')
+            }
+        })
+})
+
+function printGitbook(file) {
+    file = file.trim()
+    let unit= handleUnit(file)
+    let partB = searchPart(file, 'B. WORDS & PHRASES', 'C. LANGUAGE FOCUS')
+	partB=handleB(partB)
+
+    let outFile=`
+# Uint
+
+## WORDS & PHRASES
+單字 vocabulary|發音 pronunciation|翻譯 translation
+---|---|---
+${partB}    
+
+`
+    return outFile.trim()
+}
+
+/**
+ * 分開段落
+ * @param {String} file -the file
+ * @param {String} start -part start
+ * @param {String} end -part end
+ */
+function searchPart(file, start, end) {
+	let indexs = file.search(start)
+	let indexe = file.search(end)
+    
+	return file.slice(indexs, indexe).slice(start.length,).trim()
+}
+
+function handleUnit(file){
+    title = /- (\d+) -\n ([\w ]+)/
+    return file.match(title)
+}
+
+function handleB(str) {
+	regexp1 = /\w+ ?\w+ /g
+	regexp2 = /\((\S+)\)(\[\w?\])*\s\S+/g
+    let outStr = ''
+    
+	x = str.match(regexp1)
+	y = str.match(regexp2)
+
+	for (let i = 0; i < x.length; i++) {
+		outStr += x[i].trim() + "||" + y[i].trim() + '\n';
+    }
+    
+    return outStr;
+}
+	partA = /[M|W]:[\S ]+/g
