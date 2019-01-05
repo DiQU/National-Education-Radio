aee38824c7f35e4d358f9b5ea67a2ad22000e96b
DiQU <dieqju@gmail.com>
2019-01-06 07:15:10 +0800

    自動輸入 title

--- test.js
+++ test.js
@@ -1,29 +1,31 @@
 const fs = require('fs')
-fs.readFile('str.txt', 'utf8', (err, file) => {
-	if (err) console.log(err)
-	else
-        outFile=printGitbook(file)
-        fs.writeFile('unit.md',outFile,(err)=>{
-            if(err) console.log(err)
-            else{
+fs.readFile('unitInput.txt', 'utf8', (err, file) => {
+    if (err) console.log(err)
+    else {
+        console.log('讀取成功\n', file)
+        outFile = printGitbook(file)
+        fs.writeFile('unitOutput.md', outFile, (err) => {
+            if (err) console.log(err)
+            else {
                 console.log('寫入成功')
             }
         })
+    }
 })
 
 function printGitbook(file) {
     file = file.trim()
-    let unit= handleUnit(file)
+    let unit = handleUnit(file)
     let partB = searchPart(file, 'B. WORDS & PHRASES', 'C. LANGUAGE FOCUS')
-	partB=handleB(partB)
+    partB = handleB(partB)
 
-    let outFile=`
-# Uint
+    let outFile = `
+# Uint ${unit[1]} : ${unit[3]}
 
 ## WORDS & PHRASES
 單字 vocabulary|發音 pronunciation|翻譯 translation
 ---|---|---
-${partB}    
+${partB}
 
 `
     return outFile.trim()
@@ -36,29 +38,29 @@ ${partB}
  * @param {String} end -part end
  */
 function searchPart(file, start, end) {
-	let indexs = file.search(start)
-	let indexe = file.search(end)
-    
-	return file.slice(indexs, indexe).slice(start.length,).trim()
+    let indexs = file.search(start)
+    let indexe = file.search(end)
+
+    return file.slice(indexs, indexe).slice(start.length,).trim()
 }
 
-function handleUnit(file){
-    title = /- (\d+) -\n ([\w ]+)/
+function handleUnit(file) {
+    let title = /- (\d+) -(\r\n|\n) ([\w ?!]+)/
     return file.match(title)
 }
 
 function handleB(str) {
-	regexp1 = /\w+ ?\w+ /g
-	regexp2 = /\((\S+)\)(\[\w?\])*\s\S+/g
+    regexp1 = /\w+ ?\w+ /g
+    regexp2 = /\((\S+)\)(\[\w?\])*\s\S+/g
     let outStr = ''
-    
-	x = str.match(regexp1)
-	y = str.match(regexp2)
 
-	for (let i = 0; i < x.length; i++) {
-		outStr += x[i].trim() + "||" + y[i].trim() + '\n';
+    x = str.match(regexp1)
+    y = str.match(regexp2)
+
+    for (let i = 0; i < x.length; i++) {
+        outStr += x[i].trim() + "||" + y[i].trim() + '\n';
     }
-    
+
     return outStr;
 }
-	partA = /[M|W]:[\S ]+/g
+partA = /[M|W]:[\S ]+/g
