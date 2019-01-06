1664956b21e13fc7c8acdd9cf4c09b1363095512
DiQU <dieqju@gmail.com>
2019-01-06 22:42:54 +0800

    自動輸入 partA

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 fs.readFile('unitInput.txt', 'utf8', (err, file) => {
     if (err) console.log(err)
     else {
-        console.log('讀取成功\n', file)
+        console.log('讀取成功\n')
         outFile = printGitbook(file)
         fs.writeFile('unitOutput.md', outFile, (err) => {
             if (err) console.log(err)
@@ -16,16 +16,30 @@ fs.readFile('unitInput.txt', 'utf8', (err, file) => {
 function printGitbook(file) {
     file = file.trim()
     let unit = handleUnit(file)
+    let partA = searchPart(file, 'A. CONVERSATION', 'B. WORDS & PHRASES')
+    partA = handleA(partA)
     let partB = searchPart(file, 'B. WORDS & PHRASES', 'C. LANGUAGE FOCUS')
     partB = handleB(partB)
 
     let outFile = `
 # Uint ${unit[1]} : ${unit[3]}
 
+<audio controls>
+  <source src="https://channelplus.ner.gov.tw/api/audio/5ad2e606f95e3500064f42f3">
+</audio>
+
+## CONVERSATION
+${partA}
+
 ## WORDS & PHRASES
 單字 vocabulary|發音 pronunciation|翻譯 translation
 ---|---|---
 ${partB}
+
+## LANGUAGE FOCUS 
+<h3> ${partC1}</h3>
+
+##### 《Examples》
 
 `
     return outFile.trim()
@@ -45,8 +59,24 @@ function searchPart(file, start, end) {
 }
 
 function handleUnit(file) {
-    let title = /- (\d+) -(\r\n|\n) ([\w ?!]+)/
+    let title = /- (\d+) -(\r\n|\n) ([\S ]+)/
+    // console.log(file.match(title))
     return file.match(title)
+    
+}
+
+function handleA(str) {
+    //以'M: '或'W: '為分割點，將字串內容存入陣列 ，去除第一個空字串
+    speaker=str.match(/[MW]: /g)
+    converstion=str.split(/[MW]: /g).map(elem => elem.replace(/\r\n|\n/g,' '))
+    converstion.shift()
+    // console.log(converstion)
+    let outStr = ''
+    for(let i=0;i<speaker.length;i++){
+        outStr += '* ' +speaker[i]+converstion[i]+'\n'
+    }
+    // console.log(speaker)
+    return outStr.trim()
 }
 
 function handleB(str) {
@@ -61,6 +91,5 @@ function handleB(str) {
         outStr += x[i].trim() + "||" + y[i].trim() + '\n';
     }
 
-    return outStr;
+    return outStr.trim();
 }
-partA = /[M|W]:[\S ]+/g
